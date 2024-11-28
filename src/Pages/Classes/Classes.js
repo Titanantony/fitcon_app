ec5750d0261acdd50559d640fc7f55d89094753b
Titan antony <antonytitan96@gmail.com>
2024-11-28 22:18:25 +0300

    Chore: setup the app and the raedme file

---
+++ src/Pages/Classes/Classes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Classes() {
+  return (
+    <div>
+        <h1>Classes</h1>
+        Classes
+    </div>
+  )
+}
+
+export default Classes
---
+++ src/Pages/OnBoardingPage/OnBoardingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function OnBoardingPage() {
+  return (
+    <div>
+      <h1>OnBoardingPage</h1>
+
+    </div>
+  )
+}
+
+export default OnBoardingPage
